711b21c45eddcd0b6a2d05ae790c962227c33db5
Dorian <ddoussain@gmail.com>
2021-01-29 15:51:05 +0100

    first commit

--- screens/EventScreen.js
+++ screens/EventScreen.js
@@ -4,7 +4,7 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 const BookmarkScreen = () => {
     return (
       <View style={styles.container}>
-        <Text>Bookmark Screen</Text>
+        <Text style={{marginBottom: 10}}>Example for event cliked button</Text>
         <Button
           title="Click Here"
           onPress={() => alert('Button Clicked!')}
@@ -17,8 +17,8 @@ export default BookmarkScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
+    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center'
   },
 });
